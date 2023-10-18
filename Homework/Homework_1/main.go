39e08197b2d23763731863172b05f2dfc7746eff
DJ Tape <Dj.Tapee@yandex.ru>
2023-10-18 04:50:06 +0300

    add the test_function_add_numbers function

--- Homework/Homework_1/main.go
+++ Homework/Homework_1/main.go
@@ -4,5 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("TEST")
-	fmt.Println("ANOTHER TEST")
+	fmt.Print("ANOTHER TEST\n\n")
+	var a, b float64
+	fmt.Println("Enter two numbers to add them up: ")
+	fmt.Scanf("%g\n%g", &a, &b)
+	addition_result := test_function_add_numbers(a, b)
+	fmt.Println(a, "+", b, "is", addition_result)
+}
+
+func test_function_add_numbers(a float64, b float64) float64 {
+	return a + b
 }
